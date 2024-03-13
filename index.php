ea2710edc4a81bd7d5c13419a80683c44487ab0b
robinissacdevelop <developerrobin18@gmail.com>
2024-03-13 22:11:38 +0530

    image and X & Y componenets

--- index.php
+++ index.php
@@ -4,10 +4,16 @@ require("fpdf/fpdf.php");
 $pdf= new FPDF('P','mm','A4');//pts mm cm inches
 //a4 a3 a5 Letter Legal
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',18);
+$pdf->SetFont('Arial','B',24);
 $pdf->SetFillColor(108, 92, 231);//rgb must to fill color for background color
 $pdf->SetTextColor(223, 230, 233);//text color
-$pdf->Cell(40,10,'Hello world',1,1,'C',true);
+$pdf->Cell(80);
+$pdf->Cell(50,10,'Hello world',1,2,'C',true);
+$pdf->SetX(50);
+$pdf->SetY(30);
+$pdf->Cell(50,10,'Hello',1,2,'C',true);
 //border L-left R-right T-top B-bottom postioning L-left R-right C-center
+$pdf->Image('images/imgforpdf.jpeg',10,50,50,50,'JPG');
+//location X Y ,height,Width
 $pdf->output();
 ob_end_flush();
